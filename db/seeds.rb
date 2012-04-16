9cdae26116a9b55548e42fd361434dbfd5d79dad
Pavel Tatarsky <fazzzenda@mail.ru>
2012-04-16 19:57:24 +0400

    readme added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new
+u.email = "test@test.test"
+u.password = "123456"
+u.roles << :admin
+u.save!
